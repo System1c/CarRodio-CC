49fd70fc4a8933299383ccb4d5371a1e2f69bff8
System1c <dinerapremaratne@gmail.com>
2021-03-13 03:49:33 +0530

    implement initial dashboard

--- assets/mod/js/login.js
+++ assets/mod/js/login.js
@@ -1,6 +1,7 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const cSignin = document.getElementById('cSignIn');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -8,4 +9,8 @@ signUpButton.addEventListener('click', () => {
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+});
+
+cSignin.addEventListener('click', () => {
+	window.location.href = 'dash/index.html';
 });
